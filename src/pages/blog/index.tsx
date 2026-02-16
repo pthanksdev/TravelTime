c752bb163ca92e3e5cef915bec6b6d50cc9bf5c4
paulthanksgiving <paulthanksgiving2@gmail.com>
2026-02-16 11:27:42 +0100

    Fix typo in className for BlogPage component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,3 @@
 ﻿import { FeaturedSection, HeroSection, PostsSection, SubscribeSection } from "@/components/pages/blog/sections";
-export function BlogPage(){return <div className="space-y-8"><HeroSection /><FeaturedSection /><PostsSection /><SubscribeSection /></div>;}
+export function BlogPage(){return <div className="spa-ce-y-8"><HeroSection /><FeaturedSection /><PostsSection /><SubscribeSection /></div>;}
 
